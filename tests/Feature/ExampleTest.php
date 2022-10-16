d3554cc8c7fcaa937b369300e3cc726beb3d54b4
Silver343 <51054165+Silver343@users.noreply.github.com>
2022-10-16 21:10:34 +0100

    Initial commit

---
+++ tests/Feature/ExampleTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Tests\Feature;
+
+// use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+use Spatie\Browsershot\Browsershot;
+use Wnx\SidecarBrowsershot\BrowsershotLambda;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function test_the_application_returns_a_successful_response()
+    {
+        $content = view('welcome', [])->render();
+
+        $browsershotPdf = Browsershot::html($content)
+            ->margins(10, 18, 18, 10)
+            ->format('A4')
+            ->scale(0.8)
+            ->showBackground()
+            ->base64pdf();
+
+
+        $lambdaPdf = BrowsershotLambda::html($content)
+            ->margins(10, 18, 18, 10)
+            ->format('A4')
+            ->scale(0.8)
+            ->showBackground()
+            ->base64pdf();
+
+        $this->assertEquals($browsershotPdf,$lambdaPdf);
+    }
+}
